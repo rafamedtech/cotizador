a456d85e06952a6afc0761711c5f157ad8e0e76e
root <root@Dev>
2023-04-11 16:51:15 -0700

    protectroute test

--- server/api/invoices.get.ts
+++ server/api/invoices.get.ts
@@ -4,7 +4,7 @@ import protectRoute from '@/server/utils/protectRoute';
 const prisma = new PrismaClient();
 
 export default defineEventHandler((event) => {
-  protectRoute(event);
+  // protectRoute(event);
   return prisma.invoice.findMany({
     select: {
       invId: true,
